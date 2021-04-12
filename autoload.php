365df22dfcc3a616b06518995814671c4aab6167
angujo <angujomondi@gmail.com>
2021-04-12 13:59:44 +0300

    init

---
+++ autoload.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author       bangujo ON 2021-04-12 13:54
+ * @project      laravelmodel
+ * @ide          PhpStorm
+ * @originalFile autoload.php
+ */
+
+
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'Angujo\\LaravelModel\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
